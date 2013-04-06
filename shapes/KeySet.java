1994a55dd8947ad9232955bc4478b25308de1caa
nate00 <nathanielsullivan00@gmail.com>
2013-04-06 15:14:47 -0500

    Add docs up to KeySet

--- shapes/KeySet.java
+++ shapes/KeySet.java
@@ -5,12 +5,30 @@ import java.awt.event.*;
 /**
  * Used in calls to {@link Keyboard#direction(KeySet)} to specify which
  * direction keys to listen to. Either the arrow keys or WASD.
+ * <p>
+ * <strong>Usage example:</strong>
+ * <p>
+ * <code>
+ *  Direction inputDirection = Keyboard.direction(KeySet.WASD);<br />
+ *  circle.move(inputDirection, 10);
+ * </code>
+ * <p>
+ * This example will move <code>circle</code> ten pixels in the direction
+ * pressed on the WASD keys.
  */
 public enum KeySet {
+  /**
+   * The arrow keys.
+   */
   ARROWS(Keyboard.UP, Keyboard.DOWN, Keyboard.LEFT, Keyboard.RIGHT),
+  /**
+   * The W, A, S, and D letter keys. These are often used as secondary
+   * directional buttons, since they are laid out like the arrow keys on the
+   * left side of the keyboard.
+   */
   WASD(Keyboard.W, Keyboard.S, Keyboard.A, Keyboard.D);
 
-  public int UP, DOWN, LEFT, RIGHT;
+  int UP, DOWN, LEFT, RIGHT;
 
   private KeySet(int up, int down, int left, int right) {
     UP = up;
@@ -19,7 +37,7 @@ public enum KeySet {
     RIGHT = right;
   }
 
-  public Vector getVector(int keyCode) {
+  Vector getVector(int keyCode) {
     if (keyCode == UP)
       return Vector.UP;
     if (keyCode == DOWN)
--- shapes/Keyboard.java
+++ shapes/Keyboard.java
@@ -18,7 +18,7 @@ public class Keyboard extends KeyAdapter {
   private static Integer mostRecentKeyPressed;
   private static boolean printKeyPresses = false;
 
-  public Keyboard() {
+  Keyboard() {
     if (keysPressed == null) {
       keysPressed =
         Collections.newSetFromMap(new ConcurrentHashMap<Integer, Boolean>());
@@ -166,54 +166,193 @@ public class Keyboard extends KeyAdapter {
     }
   }
 
+  /**
+   * Key code for the letter a.
+   */
   public final static int A = 65;
+  /**
+   * Key code for the letter b.
+   */
   public final static int B = 66;
+  /**
+   * Key code for the letter c.
+   */
   public final static int C = 67;
+  /**
+   * Key code for the letter d.
+   */
   public final static int D = 68;
+  /**
+   * Key code for the letter e.
+   */
   public final static int E = 69;
+  /**
+   * Key code for the letter f.
+   */
   public final static int F = 70;
+  /**
+   * Key code for the letter g.
+   */
   public final static int G = 71;
+  /**
+   * Key code for the letter h.
+   */
   public final static int H = 72;
+  /**
+   * Key code for the letter i.
+   */
   public final static int I = 73;
+  /**
+   * Key code for the letter j.
+   */
   public final static int J = 74;
+  /**
+   * Key code for the letter k.
+   */
   public final static int K = 75;
+  /**
+   * Key code for the letter l.
+   */
   public final static int L = 76;
+  /**
+   * Key code for the letter m.
+   */
   public final static int M = 77;
+  /**
+   * Key code for the letter n.
+   */
   public final static int N = 78;
+  /**
+   * Key code for the letter o.
+   */
   public final static int O = 79;
+  /**
+   * Key code for the letter p.
+   */
   public final static int P = 80;
+  /**
+   * Key code for the letter q.
+   */
   public final static int Q = 81;
+  /**
+   * Key code for the letter r.
+   */
   public final static int R = 82;
+  /**
+   * Key code for the letter s.
+   */
   public final static int S = 83;
+  /**
+   * Key code for the letter t.
+   */
   public final static int T = 84;
+  /**
+   * Key code for the letter u.
+   */
   public final static int U = 85;
+  /**
+   * Key code for the letter v.
+   */
   public final static int V = 86;
+  /**
+   * Key code for the letter w.
+   */
   public final static int W = 87;
+  /**
+   * Key code for the letter x.
+   */
   public final static int X = 88;
+  /**
+   * Key code for the letter y.
+   */
   public final static int Y = 89;
+  /**
+   * Key code for the letter z.
+   */
   public final static int Z = 90;
 
+  /**
+   * Key code for the zero key.
+   */
   public final static int NUMBER_0 = 48;
+  /**
+   * Key code for the one key.
+   */
   public final static int NUMBER_1 = 49;
+  /**
+   * Key code for the two key.
+   */
   public final static int NUMBER_2 = 50;
+  /**
+   * Key code for the three key.
+   */
   public final static int NUMBER_3 = 51;
+  /**
+   * Key code for the four key.
+   */
   public final static int NUMBER_4 = 52;
+  /**
+   * Key code for the five key.
+   */
   public final static int NUMBER_5 = 53;
+  /**
+   * Key code for the six key.
+   */
   public final static int NUMBER_6 = 54;
+  /**
+   * Key code for the seven key.
+   */
   public final static int NUMBER_7 = 55;
+  /**
+   * Key code for the eight key.
+   */
   public final static int NUMBER_8 = 56;
+  /**
+   * Key code for the nine key.
+   */
   public final static int NUMBER_9 = 57;
 
+  /**
+   * Key code for the left arrow key.
+   */
   public final static int LEFT = 37;
+  /**
+   * Key code for the up arrow key.
+   */
   public final static int UP = 38;
+  /**
+   * Key code for the right arrow key.
+   */
   public final static int RIGHT = 39;
+  /**
+   * Key code for the down arrow key.
+   */
   public final static int DOWN = 40;
 
+  /**
+   * Key code for the space bar.
+   */
   public final static int SPACE = 32;
+  /**
+   * Key code for the enter key.
+   */
   public final static int ENTER = 10;
+  /**
+   * Key code for the escape key.
+   */
   public final static int ESC = 27;
+  /**
+   * Key code for the shift key.
+   */
   public final static int SHIFT = 16;
+  /**
+   * Key code for the backspace key.
+   */
   public final static int BACKSPACE = 8;
 
+  /**
+   * Key code representing no key. This value is returned by {@link #key} when
+   * no keys are being pressed.
+   */
   public final static int NO_KEY = -1;
 }
